1c911786a6a9f007ff395ea7bd7fc3e0f116ae18
francis-siva <fsivasithamparam@gmail.com>
2025-12-28 02:41:49 +0100

    JsonNode handling from Main class

--- src/main/java/com/codestudiocorp/Main.java
+++ src/main/java/com/codestudiocorp/Main.java
@@ -1,5 +1,11 @@
 package com.codestudiocorp;
 
+import com.codestudiocorp.controllers.JsonHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     static final String JSON_FILE = "./src/main/resources/static/input/data.json";
 
@@ -7,6 +13,18 @@ public class Main {
         System.out.println("Task scheduler wip [=>]");
 
         FileAnalyser.readFile(JSON_FILE);
+
+        try {
+            JsonNode jsonNode = FileAnalyser.objMapper.readTree(new FileReader(JSON_FILE));
+
+            JsonHandler.setNodeValue("auto maintenance", true, jsonNode, "setTaskCompleted", JSON_FILE);
+
+            FileAnalyser.serializeFile(JSON_FILE, jsonNode);
+        }
+        catch (IOException ioe) {
+            System.err.println("error cause: " + ioe.getMessage());
+            throw new RuntimeException(ioe);
+        }
         FileAnalyser.writeFile(JSON_FILE);
 
     }
